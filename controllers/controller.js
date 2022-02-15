28683f576df38b015258e269a5df829a3d8d4c09
MariusVen <marius@vengalis.co.uk>
2022-02-15 14:41:20 +0000

    solved task #14

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,18 @@
-const { fetchTopics } = require("../models/models");
+const { fetchTopics, selectArticleById } = require("../models/models");
 
 exports.getTopics = (req, res) => {
   fetchTopics().then((topics) => {
     res.status(200).send({ topics: topics });
   });
+};
+
+exports.getArticleById = (req, res, next) => {
+  const article_id = req.params.article_id;
+  selectArticleById(article_id)
+    .then((newArticle) => {
+      res.status(200).send({ article: newArticle });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
