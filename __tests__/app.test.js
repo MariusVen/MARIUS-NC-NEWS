0fd10040842c70103d7196ed84f31a20dae4bb2e
MariusVen <marius@vengalis.co.uk>
2022-04-09 21:46:52 +0100

    minor updates

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,8 +8,8 @@ afterAll(() => db.end());
 beforeEach(() => seed(data));
 
 describe("app", () => {
-  describe("GET - /api/topics", () => {
-    test("status: 200, responds with array of topic objects with slug & description properties", () => {
+  describe("GET, /api/topics", () => {
+    test("Status 200, array of topic objects", () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
@@ -25,7 +25,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 404 - responds with path not found msg for incorrect path ", () => {
+    test("Status 404, responds with path not found msg for incorrect path ", () => {
       return request(app)
         .get("/api/topikss")
         .expect(404)
@@ -35,7 +35,7 @@ describe("app", () => {
     });
   });
   describe("GET /api/articles/:article:id", () => {
-    test("status: 200 - responds with correct article object with author, title, article_id, body, topic, created_at & votes properties ", () => {
+    test("Status 200, single article object", () => {
       const article_id = 9;
       return request(app)
         .get(`/api/articles/${article_id}`)
@@ -54,7 +54,7 @@ describe("app", () => {
           );
         });
     });
-    test("status: 200 - responds with correct article including comment_count property ", () => {
+    test("Status 200, single article object (including `comment_count`) ", () => {
       const article_id = 9;
       return request(app)
         .get(`/api/articles/${article_id}`)
@@ -74,15 +74,13 @@ describe("app", () => {
           );
         });
     });
-    test(`status: 404 - responds with "path not found message" for incorrect path `, () => {
+    test(`Status 400, invalid ID, e.g. string of "not-an-id"`, () => {
       return request(app)
-        .get("/api/arcticlesz")
-        .expect(404)
-        .then((res) => {
-          expect(res.body.msg).toBe("path not found");
-        });
+        .get("/api/articles/notID")
+        .expect(400)
+        .then((res) => expect(res.body.msg).toBe("bad request"));
     });
-    test(`status: 404 - responds with "no article found" message for valid but non-existend id`, () => {
+    test(`Status 404, non existent ID, e.g. 0 or 9999`, () => {
       const article_id = 100;
       return request(app)
         .get(`/api/articles/${article_id}`)
@@ -91,15 +89,23 @@ describe("app", () => {
           expect(res.body.msg).toBe("No article found");
         });
     });
-    test(`status: 400 - responds with "bad request" message for invalid article_id`, () => {
+    test(`Status 400, responds with "bad request" message for invalid article_id`, () => {
       return request(app)
         .get("/api/articles/notID")
         .expect(400)
         .then((res) => expect(res.body.msg).toBe("bad request"));
     });
+    test(`Status 404, responds with "path not found message" for incorrect path `, () => {
+      return request(app)
+        .get("/api/arcticlesz")
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("path not found");
+        });
+    });
   });
   describe("PATCH /api/articles/:article_id", () => {
-    test("status: 200 - responds with updated article ", () => {
+    test("Status 200, updated single article object ", () => {
       const newVote = { inc_votes: 10 };
       const article_id = 9;
       return request(app)
@@ -118,7 +124,7 @@ describe("app", () => {
           });
         });
     });
-    test(`status: 404 - responds with "path not found message" for incorrect path `, () => {
+    test(`Status 404, responds with "path not found message" for incorrect path `, () => {
       return request(app)
         .patch("/api/articlesz")
         .expect(404)
@@ -126,27 +132,16 @@ describe("app", () => {
           expect(res.body.msg).toBe("path not found");
         });
     });
-    test(`status: 404 - responds with "no article found" message for valid but non-existend article id`, () => {
-      const article_id = 100;
-      const newVote = { inc_votes: 10 };
-      return request(app)
-        .patch(`/api/articles/${article_id}`)
-        .send(newVote)
-        .expect(404)
-        .then((res) => {
-          expect(res.body.msg).toBe("No article found");
-        });
-    });
-    test(`status: 400 - responds with "input missing required fields" message if input missing required fields`, () => {
-      const newVote = {};
-      const article_id = 9;
+    test(`Status 400, invalid ID, e.g. string of "not-an-id"`, () => {
+      const newVote = { inc_votes: 100 };
+      const article_id = "not-an-id";
       return request(app)
         .patch(`/api/articles/${article_id}`)
         .send(newVote)
         .expect(400)
-        .then((res) => expect(res.body.msg).toBe("missing required fields"));
+        .then((res) => expect(res.body.msg).toBe("invalid article ID"));
     });
-    test(`status: 400 - responds with "input property is incorrect type" message if input is incorrect type`, () => {
+    test(`Status 400, invalid inc_votes type, e.g. property is not a number`, () => {
       const newVote = { inc_votes: "Hello" };
       const article_id = 9;
       return request(app)
@@ -157,10 +152,30 @@ describe("app", () => {
           expect(res.body.msg).toBe("input property is incorrect type")
         );
     });
+    test(`Status 404, non existent ID, e.g. 0 or 9999`, () => {
+      const article_id = 100;
+      const newVote = { inc_votes: 10 };
+      return request(app)
+        .patch(`/api/articles/${article_id}`)
+        .send(newVote)
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("No article found");
+        });
+    });
+    test(`Status 400, responds with "input missing required fields" message if input missing required fields`, () => {
+      const newVote = {};
+      const article_id = 9;
+      return request(app)
+        .patch(`/api/articles/${article_id}`)
+        .send(newVote)
+        .expect(400)
+        .then((res) => expect(res.body.msg).toBe("missing required fields"));
+    });
   });
 
   describe("GET /api/users", () => {
-    test("status: 200 - responds with array of users objects with username property", () => {
+    test("Status 200, responds with array of users objects with username property", () => {
       return request(app)
         .get("/api/users")
         .expect(200)
@@ -175,17 +190,9 @@ describe("app", () => {
           });
         });
     });
-    test("status: 404 - responds with path not found msg for incorrect path ", () => {
-      return request(app)
-        .get("/api/userz")
-        .expect(404)
-        .then((res) => {
-          expect(res.body.msg).toBe("path not found");
-        });
-    });
   });
   describe("GET /api/articles", () => {
-    test("status: 200 - responds with array of articles objects with author, title, article_id, topic, created_at, votes properties  ", () => {
+    test("Status 200, responds with array of articles objects with author, title, article_id, topic, created_at, votes properties  ", () => {
       return request(app)
         .get("/api/articles")
         .expect(200)
@@ -205,7 +212,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - responds with array of article objects including comment_count property  ", () => {
+    test("Status 200, responds with array of article objects including comment_count property  ", () => {
       return request(app)
         .get("/api/articles")
         .expect(200)
@@ -226,7 +233,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - default sort & order: `created_at`, `desc` ", () => {
+    test("Status 200, default sort & order: `created_at`, `desc` ", () => {
       return request(app)
         .get("/api/articles")
         .expect(200)
@@ -237,7 +244,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - accepts `sort_by` query, e.g. `?sort_by=votes`", () => {
+    test("Status 200, accepts `sort_by` query, e.g. `?sort_by=votes`", () => {
       return request(app)
         .get("/api/articles?sort_by=votes")
         .expect(200)
@@ -248,7 +255,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - accepts `order` query, e.g. `?order=desc`", () => {
+    test("Status 200, accepts `order` query, e.g. `?order=desc`", () => {
       return request(app)
         .get("/api/articles?order_by=desc")
         .expect(200)
@@ -259,7 +266,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - accepts `topic` query, e.g. `?topic=cats`", () => {
+    test("Status 200, accepts `topic` query, e.g. `?topic=cats`", () => {
       return request(app)
         .get("/api/articles/?topic=cats")
         .expect(200)
@@ -279,7 +286,7 @@ describe("app", () => {
           });
         });
     });
-    test("status: 400 - invalid `sort_by` query, e.g. `?sort_by=bananas` ", () => {
+    test("Status 400, invalid `sort_by` query, e.g. `?sort_by=bananas` ", () => {
       return request(app)
         .get(`/api/articles/?sort_by=bananas`)
         .expect(400)
@@ -287,7 +294,7 @@ describe("app", () => {
           expect(res.body.msg).toBe("invalid `sort_by` query");
         });
     });
-    test("status: 400 - invalid `order` query, e.g. `?order=bananas` ", () => {
+    test("Status 400, invalid `order` query, e.g. `?order=bananas` ", () => {
       return request(app)
         .get(`/api/articles/?order_by=bananas`)
         .expect(400)
@@ -295,7 +302,7 @@ describe("app", () => {
           expect(res.body.msg).toBe("invalid `order` query");
         });
     });
-    test("status: 404 - non-existent `topic` query, e.g. `?topic=bananas`", () => {
+    test("Status 404, non-existent `topic` query, e.g. `?topic=bananas`", () => {
       return request(app)
         .get(`/api/articles/?topic=bananas`)
         .expect(404)
@@ -313,7 +320,7 @@ describe("app", () => {
     });
   });
   describe("GET /api/articles/:article_id/comments", () => {
-    test("status: 200 - responds with array of topic objects with comment_id, votes, created_at, author, body properties", () => {
+    test("Status 200, responds with array of topic objects with comment_id, votes, created_at, author, body properties", () => {
       const article_id = 9;
       return request(app)
         .get(`/api/articles/${article_id}/comments`)
@@ -333,7 +340,25 @@ describe("app", () => {
           });
         });
     });
-    test("status: 200 - responds with empty array if article exists, but doesn't have associates comments", () => {
+    test(`Status 400, invalid ID, e.g. string of "not-an-id"`, () => {
+      const article_id = "sd";
+      return request(app)
+        .get(`/api/articles/${article_id}/comments`)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.msg).toBe("bad request");
+        });
+    });
+    test(`Status 404, non existent ID, e.g. 0 or 9999`, () => {
+      const article_id = 100;
+      return request(app)
+        .get(`/api/articles/${article_id}/comments`)
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("No article found");
+        });
+    });
+    test("Status 200, valid ID, but has no comments responds with an empty array of comment", () => {
       const article_id = 2;
       return request(app)
         .get(`/api/articles/${article_id}/comments`)
@@ -343,27 +368,9 @@ describe("app", () => {
           expect(res.body.comments).toEqual([]);
         });
     });
-    test(`status: 404 - responds with "no article found" message for valid but non-existend id`, () => {
-      const article_id = 100;
-      return request(app)
-        .get(`/api/articles/${article_id}/comments`)
-        .expect(404)
-        .then((res) => {
-          expect(res.body.msg).toBe("No article found");
-        });
-    });
-    test(`status: 400 - responds with "bad request" message if article_id is not a number `, () => {
-      const article_id = "sd";
-      return request(app)
-        .get(`/api/articles/${article_id}/comments`)
-        .expect(400)
-        .then((res) => {
-          expect(res.body.msg).toBe("bad request");
-        });
-    });
   });
   describe("POST /api/articles/:article_id/comments", () => {
-    test("status: 201 - responds with added comment ", () => {
+    test("Status 201, responds with added comment ", () => {
       const article_id = 9;
       const data = { body: "testing", username: "butter_bridge" };
       return request(app)
@@ -390,7 +397,7 @@ describe("app", () => {
           );
         });
     });
-    test(`Status: 400 - invalid ID, e.g. string of "not-an-id"`, () => {
+    test(`Status 400, invalid ID, e.g. string of "not-an-id"`, () => {
       const article_id = "not-an-id";
       const data = { body: "testing", username: "butter_bridge" };
       return request(app)
@@ -401,7 +408,7 @@ describe("app", () => {
           expect(res.body.msg).toBe("bad request");
         });
     });
-    test(`status: 404 - responds with "no article found" message for valid but non-existend article id`, () => {
+    test(`Status 404, non existent ID, e.g. 0 or 9999`, () => {
       const article_id = 100;
       const data = { body: "testing", username: "butter_bridge" };
       return request(app)
@@ -413,7 +420,7 @@ describe("app", () => {
         });
     });
 
-    test(`status: 400 - missing required field(s), e.g. no username or body properties`, () => {
+    test(`Status 400, missing required field(s), e.g. no username or body properties`, () => {
       const article_id = 9;
       const data = { body: "testing" };
       return request(app)
@@ -424,18 +431,18 @@ describe("app", () => {
           expect(res.body.msg).toBe("missing username property");
         });
     });
-    // test(`status 404 - username does not exist`, () => {
-    //   const article_id = 9;
-    //   const data = { body: "testing", username: "randomUsername" };
-    //   return request(app)
-    //     .post(`/api/articles/${article_id}/comments`)
-    //     .send(data)
-    //     .expect(400)
-    //     .then((res) => {
-    //       expect(res.body.msg).toBe("username does not exist");
-    //     });
-    // });
-    test("status: 201 - ignores unnecessary properties ", () => {
+    test(`status 404, username does not exist`, () => {
+      const article_id = 9;
+      const data = { body: "testing", username: "randomUsername" };
+      return request(app)
+        .post(`/api/articles/${article_id}/comments`)
+        .send(data)
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("username does not exist");
+        });
+    });
+    test("Status 201, ignores unnecessary properties ", () => {
       const article_id = 9;
       const data = {
         body: "testing",
